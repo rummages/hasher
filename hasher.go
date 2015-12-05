7681b0858d25c63bbe159b8d0009591f00183003
rummages <e4b816@gmail.com>
2015-12-05 10:55:44 +0000

    Initial commit for hasher program to hash files

---
+++ hasher.go
@@ -0,0 +1,12 @@
+package main 
+
+import "fmt" 
+
+func main() { 
+	inputFiles := fmt.Scanf("%s", &input) // Scans stdin for space delim files to read in
+	fmt.Println(inputFiles)	
+}
+
+//func 256() {
+//
+//}	
